952331d6e7c7384a11daa17d4491b431734d2fe0
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-01-24 16:46:47 +0100

    feat: finish task2 with rank

--- Assign01/task2.sql
+++ Assign01/task2.sql
@@ -1,8 +1,14 @@
-select manager, class, sum(total) as tot_sls_dlr
+select manager, class, tot_sls_dlr, row_number() over
+(partition by manager
+order by tot_sls_dlr desc) as rank
+from
+(select manager, class, sum(total) as tot_sls_dlr
 from (select o.store_key, o.store_desc, o.manager, t.item_key, i.class, t.total
-from (select store_key, item_key, sum(tot_sls_dlr) as total
+from (
+select store_key, item_key, sum(tot_sls_dlr) as total
 from sales
-group by store_key, item_key) as T, item I, store O
-where t.item_key = i.item_key and t.store_key = o.store_key) as T1
-group by class, manager
-order by manager asc, tot_sls_dlr desc
+group by store_key, item_key
+) as T, item I, store O
+where t.item_key = i.item_key and t.store_key = o.store_key)
+group by class, manager)
+order by manager asc, class asc
