50f63e31e1c3c8568eb97b57ecb1c11ffe973cee
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-01-25 10:31:29 +0100

    minor: reformate task1

--- Assign01/task1.sql
+++ Assign01/task1.sql
@@ -1,20 +1,19 @@
-select t1.department_desc, t1.type_desc, t1.turnover as "1999", t2.turnover as "2000", t3.turnover as "2001", 100*(t2.turnover - t1.turnover) / t1.turnover as "1999-2000-increase", 100*(t3.turnover - t2.turnover) / t2.turnover as "2000-2001-increase"
+select t1.department_desc, t1.type_desc, t1.turnover as "99_reg_sls_dlr", t2.turnover as "00_reg_sls_dlr", t3.turnover as "01_reg_sls_dlr", 100*(t2.turnover - t1.turnover) / t1.turnover as "99-00-inc-percent", 100*(t3.turnover - t2.turnover) / t2.turnover as "00-01-inc-percent"
 from (
-select I.department_desc, O.type_desc, sum(reg_sls_dlr) as turnover
-from sales S, time T, item I, store O
-where S.CUR_TRN_DATE = T.CUR_TRN_DATE AND T.YEAR_KEY = 1999 AND S.ITEM_KEY = I.ITEM_KEY AND S.STORE_KEY = O.STORE_KEY
-group by I.department_desc, O.type_desc
-) as t1,
-(select I.department_desc, O.type_desc, sum(reg_sls_dlr) as turnover
-from sales S, time T, item I, store O
-where S.CUR_TRN_DATE = T.CUR_TRN_DATE AND T.YEAR_KEY = 2000 AND S.ITEM_KEY = I.ITEM_KEY AND S.STORE_KEY = O.STORE_KEY
-group by I.department_desc, O.type_desc) as t2,
-(select I.department_desc, O.type_desc, sum(reg_sls_dlr) as turnover
-from sales S, time T, item I, store O
-where S.CUR_TRN_DATE = T.CUR_TRN_DATE AND T.YEAR_KEY = 2001 AND S.ITEM_KEY = I.ITEM_KEY AND S.STORE_KEY = O.STORE_KEY
-group by I.department_desc, O.type_desc) as t3
-
-where t1.department_desc = t2.department_desc and t1.type_desc = t2.type_desc and t1.department_desc = t3.department_desc and t1.type_desc = t3.type_desc
+    select I.department_desc, O.type_desc, sum(reg_sls_dlr) as turnover
+    from sales S, time T, item I, store O
+    where S.CUR_TRN_DATE = T.CUR_TRN_DATE AND T.YEAR_KEY = 1999 AND S.ITEM_KEY = I.ITEM_KEY AND S.STORE_KEY = O.STORE_KEY
+    group by I.department_desc, O.type_desc
+    ) as t1,
+    (select I.department_desc, O.type_desc, sum(reg_sls_dlr) as turnover
+    from sales S, time T, item I, store O
+    where S.CUR_TRN_DATE = T.CUR_TRN_DATE AND T.YEAR_KEY = 2000 AND S.ITEM_KEY = I.ITEM_KEY AND S.STORE_KEY = O.STORE_KEY
+    group by I.department_desc, O.type_desc) as t2,
+    (select I.department_desc, O.type_desc, sum(reg_sls_dlr) as turnover
+    from sales S, time T, item I, store O
+    where S.CUR_TRN_DATE = T.CUR_TRN_DATE AND T.YEAR_KEY = 2001 AND S.ITEM_KEY = I.ITEM_KEY AND S.STORE_KEY = O.STORE_KEY
+    group by I.department_desc, O.type_desc) as t3
+where t1.department_desc = t2.department_desc and t1.type_desc = t2.type_desc and t1.department_desc = t3.department_desc and t1.type_desc = t3.type_desc;
 
 /*
 group reg_sls_dlr by department_desc and type_desc to calculate sum of each combination
